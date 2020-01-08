49e18f5c7094517c486f560421480cf7b4b7aeb8
Klein Florian <florian.klein@free.fr>
2020-01-08 11:37:59 +0100

    optimistic locking

--- example.sql
+++ example.sql
@@ -1,20 +1,40 @@
-begin;
+abort;
+\i schema.sql
+begin transaction isolation level serializable;
 
 select uuid_generate_v4() as user_1 \gset
 select uuid_generate_v4() as user_2 \gset
 select uuid_generate_v4() as user_3 \gset
+select uuid_generate_v4() as product_1 \gset
+select uuid_generate_v4() as product_2 \gset
 
-truncate es.events;
-truncate es.active_users;
+truncate es.event cascade;
+truncate es.user cascade;
+truncate es.product cascade;
 
-insert into es.events
-    (aggregate_id ,  added_at                 ,  type                    ,  topic   ,  payload) values
-    (:'user_1'      ,  now() + interval '1 day' ,  'user_registered'       ,  'users' ,  '{"name": "john"}'),
-    (:'user_1'      ,  now() + interval '2 day' ,  'user_changed_password' ,  'users' ,  '{"sha256": "f2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2"}'),
-    (:'user_2'      ,  now() + interval '3 day' ,  'user_registered'       ,  'users' ,  '{"name": "bob"}'),
-    (:'user_2'      ,  now() + interval '5 day' ,  'user_banned'           ,  'users' ,  '{"reason": "fail"}'),
-    (:'user_3'      ,  now() + interval '6 day' ,  'user_registered'       ,  'users' ,  '{"name": "alice"}')
-;
+insert into es.event
+(occ_version ,  aggregate_id ,  added_at                 ,  type                    ,  topic      ,  payload) values
+(1           ,  :'user_1'    ,  now() + interval '1 day' ,  'user_registered'       ,  'users'    ,  '{"name": "john"}'),
+(2           ,  :'user_1'    ,  now() + interval '2 day' ,  'user_changed_password' ,  'users'    ,  '{"sha256": "f2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2"}'),
+(3           ,  :'user_2'    ,  now() + interval '3 day' ,  'user_registered'       ,  'users'    ,  '{"name": "bob"}'),
+(4           ,  :'user_2'    ,  now() + interval '5 day' ,  'user_banned'           ,  'users'    ,  '{"reason": "fail"}'),
+(5           ,  :'user_3'    ,  now() + interval '6 day' ,  'user_registered'       ,  'users'    ,  '{"name": "alice"}'),
+(6           ,  :'product_1' ,  now() + interval '6 day' ,  'product_added'         ,  'products' ,  '{"name": "laptop","type": "electronics"}'),
+(7           ,  :'product_2' ,  now() + interval '6 day' ,  'product_added'         ,  'products' ,  '{"name": "shoe","type": "mode"}'),
+(8           ,  :'user_1'    ,  now() + interval '7 day' ,  'product_bought'        ,  'sales'    ,  jsonb_build_object(
+    'user_id',:'user_1',
+    'product_id', :'product_1'
+));
+
+with latest as (
+    select occ_version from es.event order by occ_version desc limit 1
+)
+insert into es.event
+(occ_version           ,  aggregate_id ,  added_at                 ,  type             ,  topic   ,  payload) select
+latest.occ_version + 1 ,  :'user_1'    ,  now() + interval '7 day' ,  'product_bought' ,  'sales' ,  jsonb_build_object(
+    'user_id',:'user_1',
+    'product_id',:'product_1'
+)
+from latest;
+
 commit;
-
-select * from es.active_users;
--- schema.sql
+++ schema.sql
@@ -5,55 +5,101 @@ create schema es;
 
 create extension if not exists "uuid-ossp" schema es;
 
-create table es.events (
+create table es.event (
     event_id uuid primary key default uuid_generate_v4(),
+    caused_by uuid references es.event (event_id),
+    correlation_id uuid,
     aggregate_id uuid not null,
     type text not null,
     topic text not null,
     added_at timestamptz not null default clock_timestamp(),
-    payload jsonb not null
+    payload jsonb not null,
+    occ_version bigint not null unique
 );
+create index on es.event (aggregate_id);
 
-create rule immutable_events as on update to es.events do instead nothing;
-create rule immortal_events as on delete to es.events do instead nothing;
+create rule immutable_event as on update to es.event do instead nothing;
+create rule immortal_event as on delete to es.event do instead nothing;
 
-create table es.active_users (
+create table es.user (
     user_id uuid primary key,
     name text not null,
     sha256 text,
     updated_at timestamptz not null
 );
 
-create or replace function es.trigger_project() returns trigger language plpgsql as $$
-begin
-    perform es.project(new);
-    return null;
-end;
-$$;
+create table es.product (
+    product_id uuid primary key,
+    name text not null,
+    updated_at timestamptz not null
+);
 
-create or replace function es.project(event es.events) returns void
+create table es.sale (
+    user_id uuid references es.user (user_id) on delete cascade,
+    product_id uuid references es.product (product_id) on delete cascade,
+    at timestamptz not null
+);
+
+create table es.search (
+    aggregate_id uuid primary key,
+    type text not null,
+    content tsvector not null
+);
+
+create function es.project(event es.event) returns void
 language plpgsql as $$
 begin
     case event.type
         when 'user_registered' then
-            insert into es.active_users
+            insert into es.user
             (user_id            ,  name                   ,  sha256                   ,  updated_at) values
             (event.aggregate_id ,  event.payload->>'name' ,  event.payload->>'sha256' ,  event.added_at);
+
+            insert into es.search
+            (aggregate_id, type, content) values
+            (event.aggregate_id, event.topic, to_tsvector('english', event.payload));
+
         when 'user_changed_password' then
-            update es.active_users set
+            update es.user set
             sha256 = event.payload->>'sha256',
             updated_at = event.added_at
             where user_id = event.aggregate_id;
+
         when 'user_banned' then
-            delete from es.active_users
+            delete from es.user
             where user_id = event.aggregate_id;
+
+            delete from es.search
+            where aggregate_id = event.aggregate_id;
+
+        when 'product_added' then
+            insert into es.product
+            (product_id         ,  name                   ,  updated_at) values
+            (event.aggregate_id ,  event.payload->>'name' ,  event.added_at);
+
+            insert into es.search
+            (aggregate_id, type, content) values
+            (event.aggregate_id, event.topic, to_tsvector('english', event.payload));
+
+        when 'product_bought' then
+            insert into es.sale
+            (user_id            ,  product_id                           ,  at) values
+            (event.aggregate_id ,  (event.payload->>'product_id')::uuid ,  event.added_at);
+
         else
             raise notice 'no case for event "%"', event.type;
     end case;
 end;
 $$;
 
-create trigger on_event_insert after insert on es.events
+create function es.trigger_project() returns trigger language plpgsql as $$
+begin
+    perform es.project(new);
+    return null;
+end;
+$$;
+
+create trigger on_event_insert after insert on es.event
 for each row execute function es.trigger_project();
 
 commit;
